d1e3f511d866e05016b152dd72f7b795b55f88ff
Simo Kinnunen <simo@shoqolate.com>
2014-10-17 18:57:08 +0900

    Turns out NDK 10b forces PIE. Add hack to build both PIE and non-PIE binaries.

--- jni/minirev/Android.mk
+++ jni/minirev/Android.mk
@@ -1,8 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
+# Forcefully disable PIE globally. This makes it possible to
+# build some binaries without PIE by adding the necessary flags
+# manually. These will not get reset by $(CLEAR_VARS).
+TARGET_PIE := false
+NDK_APP_PIE := false
+
 include $(CLEAR_VARS)
 
+# Enable PIE manually. Will get reset on $(CLEAR_VARS).
+LOCAL_CFLAGS += -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
+
 LOCAL_MODULE := minirev
+
+LOCAL_SRC_FILES := \
+	minirev.c \
+
+LOCAL_STATIC_LIBRARIES := uthash
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := minirev-nopie
 
 LOCAL_SRC_FILES := \
 	minirev.c \
