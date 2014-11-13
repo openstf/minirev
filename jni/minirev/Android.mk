02e52698344584ef44274f96f04f3a1824708487
Simo Kinnunen <simo@shoqolate.com>
2014-11-13 21:19:56 +0900

    No need to compile twice for PIE/non-PIE. Also, the run script now autodetects PIEness.

--- jni/minirev/Android.mk
+++ jni/minirev/Android.mk
@@ -2,16 +2,25 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := minirev-common
+
+LOCAL_SRC_FILES := \
+	minirev.c \
+
+LOCAL_STATIC_LIBRARIES := \
+	uthash \
+
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
 # Enable PIE manually. Will get reset on $(CLEAR_VARS).
 LOCAL_CFLAGS += -fPIE
 LOCAL_LDFLAGS += -fPIE -pie
 
 LOCAL_MODULE := minirev
 
-LOCAL_SRC_FILES := \
-	minirev.c \
-
-LOCAL_STATIC_LIBRARIES := uthash
+LOCAL_STATIC_LIBRARIES := minirev-common
 
 include $(BUILD_EXECUTABLE)
 
@@ -19,9 +28,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := minirev-nopie
 
-LOCAL_SRC_FILES := \
-	minirev.c \
-
-LOCAL_STATIC_LIBRARIES := uthash
+LOCAL_STATIC_LIBRARIES := minirev-common
 
 include $(BUILD_EXECUTABLE)
